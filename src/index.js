13b3bde3fdaaec1fb4147379ae9286eda33b3cf5
Kirill Smushkin <format3473@gmail.com>
2024-01-15 18:46:24 +0200

    before close popup function changed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const profileEditButton = document.querySelector(".profile__edit-button");
 const popupTypeEdit = document.querySelector(".popup_type_edit");
 const profileAddButton = document.querySelector(".profile__add-button");
 const popupTypeNewCard = document.querySelector(".popup_type_new-card");
-const newCardForm = popupTypeNewCard.querySelector('.popup__form')
 const typeEditForm = popupTypeEdit.querySelector(".popup__form");
 const nameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_description");
@@ -34,19 +33,18 @@ initialCards.forEach(function (initialCard) {
 //попап редактированя профиля
 
 profileEditButton.addEventListener("click", function (evt) {
-  openPopup(popupTypeEdit)
+  openPopup(popupTypeEdit);
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
 });
 
 //попап добавления карточки
 
 profileAddButton.addEventListener("click", function (evt) {
-  openPopup(popupTypeNewCard)
+  openPopup(popupTypeNewCard);
 });
 
 //редактирование профиля
-
-nameInput.value = profileTitle.textContent;
-jobInput.value = profileDescription.textContent;
 
 function handleEditProfileForm(evt) {
   evt.preventDefault();
@@ -64,16 +62,16 @@ typeEditForm.addEventListener("submit", handleEditProfileForm);
 function addNewCard(evt) {
   evt.preventDefault();
 
-  const newCardObject = [{
-    name: popupInputTypeCardName.value,
-    link: popupInputTypeUrl.value,
-  }];
+  const newCardObject = {
+      name: popupInputTypeCardName.value,
+      link: popupInputTypeUrl.value,
+    };
 
-  placesList.prepend(addCard(newCardObject[0], deleteCard, openCardImage, likeCard));
+  placesList.prepend(
+    addCard(newCardObject, deleteCard, openCardImage, likeCard)
+  );
   popupTypeNewCard.classList.remove("popup_is-opened");
-  newCardForm.reset()
+  newPlaceForm.reset();
 }
 
 newPlaceForm.addEventListener("submit", addNewCard);
-
-
